df19fd343b7f71f73d630eb447aa73c5aa8c2b70
Thomas Lugrin <35079233+Dsobiblu@users.noreply.github.com>
2021-10-22 12:35:07 +0200

    Remove test chunk

--- main.cpp
+++ main.cpp
@@ -11,12 +11,4 @@ int main(int argc, char* argv[])
     mdpWindow.show();
 
     return app.exec();
-    /*QChar a;
-    for (int i=33; i<128; i++) {
-        a = QChar(i);
-        //if (a.category() == QChar::Other_Control || a.category() == QChar::Symbol_Modifier) {
-            std::cout << a.toLatin1() << " " << a.category() << ", ";
-        //}
-    }
-    std::cout << std::endl;*/
 }
